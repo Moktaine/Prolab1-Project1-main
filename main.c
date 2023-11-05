73a491d69d95294149608edf8a39055ac7f4372a
Moktaine <alperalk2002@hotmail.com>
2023-11-05 09:10:59 +0300

    c

--- main.c
+++ main.c
@@ -11,20 +11,24 @@ int polygonCount = 0;
 int polygonVertexCounts[3];
 int squareCounts[3];
 
-//Check if variables are already printed so they don't get printed several times
+//Boolean for Checking if variables are already printed so they don't get printed several times
 bool IsVariablesPrinted = false;
 
+//Values to be printed
 int platformCount = 0, drillCount = 0;
 int drillCost, platformCost;
 int totalArea = 0;
 
+//Coordinate Structure
 struct Coordinate{
     int x;
     int y;
 };
 
+//2D Array for storing vertex coordinates of polygons
 struct Coordinate polygons[3][10];
 
+//2D Arrays for storing every unit square inside polygon areas
 struct Coordinate SquaresInsidePolygon[3][500];
 struct Coordinate Default_SquaresInsidePolygon[3][500];
 
@@ -33,8 +37,8 @@ struct Coordinate Default_SquaresInsidePolygon[3][500];
 
 
 
+//Returns the index of the given unit square in SquaresInsidePolygon array
 int SquareIndexInArray(int x, int y, int polygonIndex){
-    //Returns the index of the given unit square in SquaresInsidePolygon array
     for(int i = 0; i < squareCounts[polygonIndex]; i++){
         if(SquaresInsidePolygon[polygonIndex][i].x == x && SquaresInsidePolygon[polygonIndex][i].y == y){
             return i;
@@ -43,8 +47,9 @@ int SquareIndexInArray(int x, int y, int polygonIndex){
     return -1;
 }
 
+
+//Checks if any unit square inside given square is either not inside polygon or occupied
 bool CanDrawSquare(int x, int y, int edgeLength, int polygonIndex){
-    //Checks if any unit square inside given square is invalid
     for(int i = 0; i < edgeLength; i++){
         for(int j = 0; j < edgeLength; j++){
             int checkX = x + (j*10);
@@ -57,8 +62,10 @@ bool CanDrawSquare(int x, int y, int edgeLength, int polygonIndex){
     return true;
 }
 
+
+//Tags every unit square inside given square as occupied
 void RemoveSquareFromSquareArray(int x, int y, int edgeLength, int polygonIndex){
-    //Removes every unit square inside given square
+
     for(int i = 0; i < edgeLength; i++){
         for(int j = 0; j < edgeLength; j++){
             int removeX = x + (j*10);
@@ -71,10 +78,12 @@ void RemoveSquareFromSquareArray(int x, int y, int edgeLength, int polygonIndex)
     }
 }
 
+
+
+//Draws the square with given edge length
 void DrawBigSquare(int x, int y, int edgeLength, int polygonIndex,bool isNeighbour){
     //Checks if given square can be drawn and if so draw
     if(CanDrawSquare(x,y,edgeLength/10,polygonIndex)){
-        y = y ;//+ (window_HEIGHT/2);
         //Set Color
 
         switch(edgeLength/10){
@@ -114,11 +123,13 @@ void DrawBigSquare(int x, int y, int edgeLength, int polygonIndex,bool isNeighbo
 
             platformCount++;
 
-        RemoveSquareFromSquareArray(x, y /*- (window_HEIGHT/2)*/, edgeLength/10, polygonIndex);
+        RemoveSquareFromSquareArray(x, y , edgeLength/10, polygonIndex);
     }
 }
 
 
+
+//Checks if a big square's edges are occupied or not valid for drawing the neighbour square
 bool CanDrawBigSquare(int x, int y, int edgeLength, int polygonIndex){
     edgeLength = edgeLength * 10;
 
@@ -203,6 +214,9 @@ bool CanDrawBigSquare(int x, int y, int edgeLength, int polygonIndex){
     return true;
 }
 
+
+
+//Draws the neighbour squares of given big square
 void DrawNeighbourSquares(int x, int y, int edgeLength, int polygonIndex){
     int newEdgeLength = edgeLength/2;
     int squaresPerEdge = edgeLength/10;
@@ -277,17 +291,21 @@ void DrawNeighbourSquares(int x, int y, int edgeLength, int polygonIndex){
 
 }
 
+
+
+//Checks every big square and finds how to draw least amount of square to fully cover the area
 void CheckBigSquares(int squaresPerEdge){
     int edgeLength = squaresPerEdge * 10;
 
-    struct Coordinate TempSquaresInsidePolygon[3][600];
-    struct Coordinate DrawableSquares[200];
+    struct Coordinate TempSquaresInsidePolygon[3][500];
+    struct Coordinate DrawableSquares[400];
     int squareCount;
 
 
 
     for(int i = 0; i < polygonCount; i++){
         squareCount = 0;
+
 
         //Find Drawable Big Squares
         for(int j = 0; j < squareCounts[i]; j++){
@@ -316,15 +334,18 @@ void CheckBigSquares(int squaresPerEdge){
         int squareCounter;
 
 
+        //Sets the TempSquaresInsidePolygon
         for(int j = 0; j < squareCounts[i]; j++){
             TempSquaresInsidePolygon[i][j].x = SquaresInsidePolygon[i][j].x;
             TempSquaresInsidePolygon[i][j].y = SquaresInsidePolygon[i][j].y;
         }
 
 
+        //Checks every big square index to find most amount of big square that can be drawn at the same time
         for(int j = 0; j < squareCount; j++){
             squareCounter = 0;
 
+            //Checks squares after index
             for(int k = j; k < squareCount; k++){
                 int currentX = DrawableSquares[k].x;
                 int currentY = DrawableSquares[k].y;
@@ -334,7 +355,9 @@ void CheckBigSquares(int squaresPerEdge){
                     RemoveSquareFromSquareArray(DrawableSquares[k].x, DrawableSquares[k].y, squaresPerEdge,i);
                 }
             }
-             for(int k = j; k >= 0; k--){
+
+            //Checks squares before index
+            for(int k = j; k >= 0; k--){
                 int currentX = DrawableSquares[k].x;
                 int currentY = DrawableSquares[k].y;
 
@@ -344,11 +367,14 @@ void CheckBigSquares(int squaresPerEdge){
                 }
             }
 
+            //Checks if counter is the new max count
             if(squareCounter > maxSquareCount){
                 maxSquareCount = squareCounter;
                 maxSquareCountIndex = j;
             }
 
+
+            //Resets the SquaresInsidePolygon array
             for(int k = 0; k < squareCounts[i]; k++){
                 SquaresInsidePolygon[i][k].x = TempSquaresInsidePolygon[i][k].x;
                 SquaresInsidePolygon[i][k].y = TempSquaresInsidePolygon[i][k].y;
@@ -358,12 +384,16 @@ void CheckBigSquares(int squaresPerEdge){
         struct Coordinate DrawnSquares[200];
         int drawnSquareCount = 0;
 
+
+        //Draws the first square in maxSquareCountIndex to make sure first one is drawn
         DrawBigSquare(DrawableSquares[maxSquareCountIndex].x, DrawableSquares[maxSquareCountIndex].y, edgeLength, i, false);
         DrawnSquares[drawnSquareCount].x = DrawableSquares[maxSquareCountIndex].x;
         DrawnSquares[drawnSquareCount].y = DrawableSquares[maxSquareCountIndex].y;
         drawnSquareCount++;
 
 
+
+        //Draws squares after maxSquareCountIndex
         for(int j = maxSquareCountIndex; j < squareCount; j++){
             int currentX = DrawableSquares[j].x;
             int currentY = DrawableSquares[j].y;
@@ -377,6 +407,8 @@ void CheckBigSquares(int squaresPerEdge){
 
         }
 
+
+        //Draws squares before maxSquareCountIndex
         for(int j = maxSquareCountIndex; j >= 0; j--){
             int currentX = DrawableSquares[j].x;
             int currentY = DrawableSquares[j].y;
@@ -392,8 +424,7 @@ void CheckBigSquares(int squaresPerEdge){
 
 
 
-        //Draw the neighbour squares
-
+        //Draws the neighbour squares
         for(int j = 0; j < squareCount; j++){
             int currentX = DrawnSquares[j].x;
             int currentY = DrawnSquares[j].y;
@@ -405,33 +436,42 @@ void CheckBigSquares(int squaresPerEdge){
 
 
 
-///////////////////////////////////////////////////////////////////////////////////
-
+//Checks if a point is inside polygon
 bool IsPointInsidePolygonRayTrace(GLint xPoint, GLint yPoint, int polygonIndex){
     float x1Line,x2Line,y1Line,y2Line, x0;
     int count = 0;
     bool IsPointOnEdge = false;
 
+
+    //Checks how many edges crossed with the line that goes straight towards right from initial point
     for(int i = 0; i < polygonVertexCounts[polygonIndex]; i++){
 
-        //Set edge vertices
+        //Sets coordinates of the first vertex of the edge line
         x1Line = polygons[polygonIndex][i].x;
         y1Line = polygons[polygonIndex][i].y;
 
+        //If the point is on line's first vertex, count as 1
         if(xPoint == x1Line && yPoint == y1Line){
             count = 1;
             break;
         }else{
+            //If for-loop is on last vertex of polygon set next vertex as first vertex
             if((i == polygonVertexCounts[polygonIndex] - 1)){
                 x2Line = polygons[polygonIndex][0].x;
                 y2Line = polygons[polygonIndex][0].y;
             }else{
+            //If not, set second vertex of the line as next vertex of polygon
                 x2Line = polygons[polygonIndex][i+1].x;
                 y2Line = polygons[polygonIndex][i+1].y;
             }
 
+
+            //x0 is the distance between x of the first vertex of the line and the crossing point of the
+            //line that goes straight towards right from the initial point and the edge line
             x0 = x1Line + ( (y1Line - yPoint) / (y1Line - y2Line) ) * (x2Line-x1Line);
 
+
+            //Checks if initial point is between to lines' y values and xPoint <= 0
             if( ( (yPoint < y1Line) != (yPoint < y2Line) ) && xPoint <= x0 ){
                 if(xPoint == x0){IsPointOnEdge = true;}
                 count++;
@@ -439,6 +479,7 @@ bool IsPointInsidePolygonRayTrace(GLint xPoint, GLint yPoint, int polygonIndex){
         }
     }
 
+    //If the initial point is on a edge don't count the point as inside
     if(IsPointOnEdge && count == 1){
         return false;
     }
@@ -447,6 +488,8 @@ bool IsPointInsidePolygonRayTrace(GLint xPoint, GLint yPoint, int polygonIndex){
 }
 
 
+
+//Checks every corner of the unit square if any corner is inside polygon square counts as inside polygon
 bool IsSquareInsidePolygon(GLint topLeftX, GLint topLeftY, int polygonIndex){
 
     if(IsPointInsidePolygonRayTrace(topLeftX, topLeftY, polygonIndex)){return true;}
@@ -458,6 +501,8 @@ bool IsSquareInsidePolygon(GLint topLeftX, GLint topLeftY, int polygonIndex){
 
 }
 
+
+//Finds the min and max values of x and y in vertices of a polygon
 struct Coordinate *FindEdgeVertices(int polygonIndex){
     //Find the min and max x and y values
     int minX = 0, maxiX = 0, minY = 0, maxiY = 0;
@@ -503,6 +548,8 @@ struct Coordinate *FindEdgeVertices(int polygonIndex){
     return edgeVertices;
 }
 
+
+//Checks every vertex in given array and splits the vertices in to polygons
 void splitPolygonVertices(struct Coordinate vertices[], int vertexCount){
     //Create polygons array
     struct Coordinate startVertex = vertices[0];
@@ -525,6 +572,7 @@ void splitPolygonVertices(struct Coordinate vertices[], int vertexCount){
             //Set polygon count and store vertex count of all polygons to array
             polygonVertexCounts[polygonCount] = polygonEndIndex - polygonStartIndex;
             polygonCount++;
+
             //Set new start vertex
             polygonStartIndex = i;
             startVertex = vertices[i];
@@ -534,6 +582,8 @@ void splitPolygonVertices(struct Coordinate vertices[], int vertexCount){
 
 }
 
+
+//Gets a string and finds the vertex coordinate values and stores them in an array and calls splitPolygonVertices
 void setCoordinates(char *buffer){
     int searchStartIndex = 0;
 
@@ -586,9 +636,11 @@ void setCoordinates(char *buffer){
                 vertexCount++;
                 IsAssigningCoordinate_x = true;
             }
+
             //Reset "coordinateString" string
             memset(coordinateString, '\0', sizeof(coordinateString));
             coordinateInt = 0;
+
             //Reset Start and End Index
             numberStartIndex = -1;
             numberEndIndex = -1;
@@ -596,11 +648,13 @@ void setCoordinates(char *buffer){
 
     }
 
-    //Split the vertices to polygons
+    //Call the function to split the vertices to polygons
     splitPolygonVertices(vertices, vertexCount);
 
 }
 
+
+//Determines and draws shapes in first window
 void renderScene(void){
     //Set Background color
     glClearColor(1,1,1,1);
@@ -609,11 +663,8 @@ void renderScene(void){
     //Set origin point to top left corner
     //{
     glViewport(0,0, window_WIDTH, window_HEIGHT);
-    //----------------------------------------------------------glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(0,window_WIDTH, window_HEIGHT, 0,-1,1);
-    //glMatrixMode(GL_MODELVIEW);
-    //glLoadIdentity();
     //}
 
     //Draw Square grid
@@ -680,6 +731,8 @@ void renderScene(void){
 }
 
 
+
+//Determines and draws shapes in second window
 void renderSecond(void){
       //Set Background color
     glClearColor(1,1,1,1);
@@ -713,6 +766,7 @@ void renderSecond(void){
         }
     }
 
+    //Calls the function that splits polygons into big squares
     CheckBigSquares(16);
     CheckBigSquares(8);
     CheckBigSquares(4);
@@ -720,7 +774,7 @@ void renderSecond(void){
     CheckBigSquares(1);
 
 
-    //Print the Results
+    //Prints the Results
     if(!IsVariablesPrinted){
         int profit, totalDrillCost, totalPlatformCost, sourceValue, totalCost;
 
@@ -749,7 +803,7 @@ void renderSecond(void){
 
 
 
-
+//Writes data from contents to output
 size_t curl_write(void *contents, size_t size, size_t nmemb, char **output) {
     size_t total_size = size * nmemb;
     *output = (char *)malloc(total_size + 1);
@@ -761,6 +815,7 @@ size_t curl_write(void *contents, size_t size, size_t nmemb, char **output) {
 }
 
 
+//Reads and returns a specific line from the given URL
 char * ReadLineFromURL(){
     CURL *curl = curl_easy_init();
 
@@ -814,24 +869,20 @@ int main(int argc, char **argv)
 {
 
     //Get Cost Inputs
-
     printf("Birim sondaj maliyetini giriniz: ");
     scanf("%d", &drillCost);
 
     printf("Birim platform maliyetini giriniz: ");
     scanf("%d", &platformCost);
 
-    //Set Coordinates
-    char testText[100] = "1B(5,10)(20,5)(30,12)(15,40)(5,10)(60,5)(90,20)(80,25)(50,4ssssssssssssssssss0)(60,5)F";
 
-    //setCoordinates(ReadLineFromURL());
-    setCoordinates(testText);
+    //Read URL and set coordinates of the polygon
+    setCoordinates(ReadLineFromURL());
 
 
-    //CREATE WINDOW AND DISPLAY THE SCENE
+    //CREATE WINDOWS AND DISPLAY THE SCENES
     //{
 
-    GLint WindowID1, WindowID2;
     //Create window
     glutInit(&argc, argv);
 
@@ -839,18 +890,20 @@ int main(int argc, char **argv)
     glutInitDisplayMode(GLUT_RGB | GLUT_ALPHA);
     glutInitWindowSize(1500,window_HEIGHT);
     glutInitWindowPosition(50,50);
-    WindowID1 = glutCreateWindow("Window1");
+    glutCreateWindow("Window1");
 
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable( GL_BLEND );
 
     glutDisplayFunc(renderScene);
 
+
+
     //Create Second Window
     glutInitDisplayMode(GLUT_RGB | GLUT_ALPHA);
     glutInitWindowSize(1500,window_HEIGHT);
     glutInitWindowPosition(300,50);
-    WindowID2 = glutCreateWindow("Window2");
+    glutCreateWindow("Window2");
 
     //Set blend function to control transparency via alpha channel
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
